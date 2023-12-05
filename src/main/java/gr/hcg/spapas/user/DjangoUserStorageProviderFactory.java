00e253e2ad04916590d268c75a8e3d665b60cb8b
Serafeim Papastefanos <spapas@gmail.com>
2023-12-05 20:38:48 +0200

    Fixes

--- src/main/java/gr/hcg/spapas/user/DjangoUserStorageProviderFactory.java
+++ src/main/java/gr/hcg/spapas/user/DjangoUserStorageProviderFactory.java
@@ -9,6 +9,7 @@ import org.keycloak.provider.ProviderConfigProperty;
 import org.keycloak.provider.ProviderConfigurationBuilder;
 import org.keycloak.storage.UserStorageProviderFactory;
 
+import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,12 +19,7 @@ import java.util.Map;
  */
 public class DjangoUserStorageProviderFactory implements UserStorageProviderFactory<DjangoUserStorageProvider> {
     private static final Logger logger = Logger.getLogger(DjangoUserStorageProviderFactory.class);
-
-    private Map<String, ProviderConfig> dataSourcePerInstance = new HashMap<>();
-
-    private static class ProviderConfig {
-        private HikariDataSource ds = null;
-    }
+    private Map<String, DataSource> dataSourcePerInstance = new HashMap<>();
 
     @Override
     public void init(Config.Scope config) {
@@ -32,35 +28,24 @@ public class DjangoUserStorageProviderFactory implements UserStorageProviderFact
 
     @Override
     public DjangoUserStorageProvider create(KeycloakSession session, ComponentModel model) {
-        System.err.println("Create");
-        ProviderConfig pc = dataSourcePerInstance.computeIfAbsent(model.getId(),
+        DataSource ds = dataSourcePerInstance.computeIfAbsent(model.getId(),
                 s -> {
-                    System.err.println("Create 2");
-                    HikariDataSource ds = new HikariDataSource();
+                    HikariDataSource newds = new HikariDataSource();
                     String url = "jdbc:mysql://user:user@127.0.0.1:3306/db";
-                    System.err.println(0);
                     try {
-                        System.err.println(1);
-                        System.err.println(model.get("jdbcUrlEnv"));
                         url = System.getenv(model.get("jdbcUrlEnv"));
-                        System.err.println(url);
-                        System.err.println(2);
                         if (url == null || url.isEmpty()) {
-                            System.err.println(3);
+
                             logger.error("JDBC URL not found!");
                         }
                     } catch (Exception e) {
-                        System.err.println(4);
                         e.printStackTrace();
                     }
-                    System.err.println(5);
-                    ds.setJdbcUrl(url);
-                    ProviderConfig dsConfig = new ProviderConfig();
-                    dsConfig.ds = ds;
-                    return dsConfig;
+                    newds.setJdbcUrl(url);
+                    return newds;
                 }
         );
-        return new DjangoUserStorageProvider(session, model, pc.ds);
+        return new DjangoUserStorageProvider(session, model, ds);
 
     }
 
@@ -72,7 +57,13 @@ public class DjangoUserStorageProviderFactory implements UserStorageProviderFact
     @Override
     public List<ProviderConfigProperty> getConfigProperties() {
         return ProviderConfigurationBuilder.create()
-                .property("jdbcUrlEnv", "Environment var with JDBC URL", "Please enter the name of an env var that contains the JDBC URL of the connection", ProviderConfigProperty.STRING_TYPE, "REGUSR_JDBC_URL", null)
+                .property()
+                .name("jdbcUrlEnv")
+                .label("Environment var with JDBC URL")
+                .helpText("Please enter the name of an env var that contains the JDBC URL of the connection")
+                .type(ProviderConfigProperty.STRING_TYPE)
+                .defaultValue("REGUSR_JDBC_URL")
+                .add()
                 .build();
     }
 }
